8855b48d3f8c68fff1c76f78d5fac919ad5d6941
ismaelayyad <59508930+ismaelayyad@users.noreply.github.com>
2022-04-20 02:27:22 +0300

    Add files via upload

---
+++ معروف_files/in.php
@@ -0,0 +1,7 @@
+// (☞ﾟ∀ﾟ)☞ static31
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '532399790');
+var unpoco_100935705=1;
+_cgen.set_cookie('_no_tracky_100935705', 1, 3600, 1 );
+}
+// bad site id (╯°□°）╯︵ ┻━┻
